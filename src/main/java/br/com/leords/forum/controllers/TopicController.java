6dbcf6f8b109109a9821f7a111de10f2b01541a2
Leonardo Ramos de Sousa <leonardords@ciandt.com>
2022-04-11 13:28:05 -0300

    validation with bean validation

--- src/main/java/br/com/leords/forum/controllers/TopicController.java
+++ src/main/java/br/com/leords/forum/controllers/TopicController.java
@@ -2,10 +2,15 @@ package br.com.leords.forum.controllers;
 
 import br.com.leords.forum.controllers.dtos.TopicDto;
 import br.com.leords.forum.controllers.forms.TopicForm;
+import br.com.leords.forum.models.Topic;
 import br.com.leords.forum.repositories.CourseRepository;
 import br.com.leords.forum.repositories.TopicRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,7 +34,10 @@ public class TopicController {
     }
     
     @PostMapping
-    public void createTopic(@RequestBody TopicForm form) {
-        topicRepository.save(form.formToModel(courseRepository));
+    public ResponseEntity<TopicDto> createTopic(@RequestBody @Valid TopicForm form, UriComponentsBuilder uriBuilder) {
+        Topic topic = form.formToModel(courseRepository);
+        topicRepository.save(topic);
+        URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicDto(topic));
     }
 }
--- src/main/java/br/com/leords/forum/controllers/forms/TopicForm.java
+++ src/main/java/br/com/leords/forum/controllers/forms/TopicForm.java
@@ -2,11 +2,23 @@ package br.com.leords.forum.controllers.forms;
 
 import br.com.leords.forum.models.Topic;
 import br.com.leords.forum.repositories.CourseRepository;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 public class TopicForm {
     
+    @NotNull
+    @NotEmpty
+    @Length(min = 5)
     private String title;
+    @NotNull
+    @NotEmpty
+    @Length(min = 10)
     private String message;
+    @NotNull
+    @NotEmpty
     private String courseName;
     
     public Topic formToModel(CourseRepository courseRepository) {
