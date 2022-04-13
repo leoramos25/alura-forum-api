fad7102e87e3b8a8e3361af6539eff15396dc687
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-04-12 22:28:30 -0300

    enabled spring web security

---
+++ src/main/java/br/com/leords/forum/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package br.com.leords.forum.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
