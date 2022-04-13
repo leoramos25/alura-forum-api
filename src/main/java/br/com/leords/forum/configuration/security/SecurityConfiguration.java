bc8fca755ab9628f0bf36d0c928b6a53143fb077
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-04-12 22:44:00 -0300

    releasing acess from http get methods

--- src/main/java/br/com/leords/forum/configuration/security/SecurityConfiguration.java
+++ src/main/java/br/com/leords/forum/configuration/security/SecurityConfiguration.java
@@ -1,10 +1,38 @@
 package br.com.leords.forum.configuration.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final static String[] AUTH_WHITELIST = {
+            "/topics",
+            "/topics/*"
+    };
+    
+    //Authentication config
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+    
+    //Authorization config
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, AUTH_WHITELIST)
+                .permitAll();
+    }
+    
+    //Static resources configuration
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 }
